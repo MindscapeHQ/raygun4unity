153fad9721675cd2b0b44c1eca469933598a748e
Jason Fauchelle <jason@mindscape.co.nz>
2014-06-24 16:25:03 +1200

    Initial code commit

---
+++ Mindscape.Raygun4Unity/IRaygunMessageBuilder.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Mindscape.Raygun4Unity.Messages;
+
+namespace Mindscape.Raygun4Unity
+{
+  public interface IRaygunMessageBuilder
+  {
+    RaygunMessage Build();
+
+    IRaygunMessageBuilder SetMachineName(string machineName);
+
+    IRaygunMessageBuilder SetExceptionDetails(string message, string stackTrace, string type);
+
+    IRaygunMessageBuilder SetClientDetails();
+
+    IRaygunMessageBuilder SetEnvironmentDetails();
+
+    IRaygunMessageBuilder SetVersion(string version);
+
+    IRaygunMessageBuilder SetUserCustomData(IDictionary userCustomData);
+
+    IRaygunMessageBuilder SetTags(IList<string> tags);
+
+    IRaygunMessageBuilder SetUser(string user);
+  }
+}
