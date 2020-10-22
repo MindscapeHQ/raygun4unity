6453c37208ceb253012f1071b8bc2eb49ff0a9c1
Mitchell Duncan <mitchell@raygun.com>
2020-10-22 14:56:19 +1300

    Adding service to handle sending web requests
    Adding service to handle sending web requests

---
+++ Mindscape.Raygun4Unity/SingletonBehaviour.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+
+namespace Mindscape.Raygun4Unity
+{
+  public class SingletonBehaviour<T> : MonoBehaviour where T : MonoBehaviour
+  {
+    private static T _instance;
+    private static object _lock = new object();
+    private static bool _applicationIsQuitting = false;
+
+    public static T Instance
+    {
+      get
+      {
+        if (_applicationIsQuitting) 
+        {
+          return null;
+        }
+  
+        lock(_lock)
+        {
+          if (_instance == null)
+          {
+            _instance = (T)FindObjectOfType(typeof(T));
+          
+            if (_instance == null)
+            {
+              GameObject singleton = new GameObject();
+              _instance = singleton.AddComponent<T>();
+              singleton.name = typeof(T).ToString();
+
+              DontDestroyOnLoad(singleton);
+            }
+          }
+          return _instance;
+        }
+      }
+    }
+
+    public virtual void Awake() 
+    {
+      DontDestroyOnLoad(this.gameObject);
+    }
+
+    public void OnDestroy()
+    {
+      _applicationIsQuitting = true;
+    }
+  }
+}
